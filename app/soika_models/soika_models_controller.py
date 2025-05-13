7fe45b59c4e1d4d8bcbf3255326c7dc247d78c83
voronapxl <trololo695@gmail.com>
2025-05-13 13:48:59 +0300

    feat(post): added request model for post methods

--- app/soika_models/soika_models_controller.py
+++ app/soika_models/soika_models_controller.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from .emotions_request_model import TextPayload
 from .soika_models_service import soika_models_service
 
 
@@ -7,13 +8,11 @@ soika_models_controller = APIRouter(prefix="/soika_models", tags=["Soika Models"
 
 
 @soika_models_controller.post("/extract_emotions")
-async def get_emotions(text: str):
-
-    result = await soika_models_service.extract_emotions(text)
+async def get_emotions(payload: TextPayload):
+    result = await soika_models_service.extract_emotions(payload.text)
     return result
 
 @soika_models_controller.post("/extract_ner")
-async def get_ner(text: str):
-
-    result = await soika_models_service.extract_ner(text)
+async def get_ner(payload: TextPayload):
+    result = await soika_models_service.extract_ner(payload.text)
     return result
